9153f07d2993732097f9f8c289c238a6367634aa
amits <amits@harmon.ie>
2022-05-30 13:15:40 +0300

    Fix: put code inside try catch

--- packages/servers/serverless/organization_getLicense/index.ts
+++ packages/servers/serverless/organization_getLicense/index.ts
@@ -7,22 +7,22 @@ const httpTrigger: AzureFunction = async function (
   context: Context,
   req: HttpRequest
 ): Promise<void> {
-  const { log } = context;
-  const { appConfig, reqValidationResults } = await AppLoader.initApp(
-    req,
-    true
-  );
-  if (reqValidationResults.status !== 200) {
-    context.res = {
-      status: reqValidationResults.status,
-      body: reqValidationResults.message,
-    };
-    return;
-  }
-  const organizationController = new OrganizationController(appConfig, log);
   let response = {};
   if (req?.query?.tid) {
     try {
+      const { log } = context;
+      const { appConfig, reqValidationResults } = await AppLoader.initApp(
+        req,
+        true
+      );
+      if (reqValidationResults.status !== 200) {
+        context.res = {
+          status: reqValidationResults.status,
+          body: reqValidationResults.message,
+        };
+        return;
+      }
+      const organizationController = new OrganizationController(appConfig, log);
       response = await organizationController.getLicense(req.query.tid);
     } catch (error: any) {
       const errorMessage: ErrorResponse = {
